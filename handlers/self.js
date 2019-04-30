d98f19c1d64b06b022be48f452a49a11858a26ec
Sean Macdonald <sean@crazyhorsecoding.com>
2019-04-30 00:19:57 -0400

    wip

---
+++ handlers/self.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+var SerfRPC = require("serf-rpc");
+var serf = new SerfRPC();
+
+const p = () => {
+	return new Promise((resolve, reject) => {
+		serf.connect(function (err) {
+			if (err) {
+				reject(err);
+			}
+			serf.stats((err, me) => {
+				const r = {
+					"name": me.agent.name
+				};
+				for (let k in me.tags) {
+					if (/cabal/.test(k)) {
+						let shortKey = k.replace('ca.fukt.cabal/', '');
+						let v = me.tags[k];
+						r[shortKey] = v;
+					}
+				}
+				resolve(r);
+			});
+			serf.stop();
+		});
+	});
+};
+
+module.exports = p;
