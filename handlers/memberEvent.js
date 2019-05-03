aab6e07ad16771d19ddc85a1605034c5c4c080d9
Sean Macdonald <sean@crazyhorsecoding.com>
2019-05-02 20:34:31 -0400

    cool dude

--- handlers/memberEvent.js
+++ handlers/memberEvent.js
@@ -1,6 +1,22 @@
 const notifier = require('node-notifier');
 
 const fn = (msg) => {
+	var message;
+	switch (msg.event) {
+		case 'member-join':
+		message = `${msg.node.name} just joined`;
+		break;
+		case 'member-leave':
+		message = `${msg.node.name} just left`;
+		break;
+		case 'member-update':
+		message = `${msg.node.name} was updated`;
+		break;
+		default:
+		message = `${msg.node.name} just did ${msg.event}`;
+		break;
+	}
+	//	notify operator
 	notifier.notify({
 		title: msg.event,
 		message: `${msg.node.name} just did a ${msg.event}`
