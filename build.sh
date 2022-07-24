bce47cee3eb020725b6e4218e228191aa50d2db4
Sean Macdonald <sean.macdonald@rmp.uhn.ca>
2022-07-24 17:17:37 -0400

    tighter Makefile integration

--- build.sh
+++ build.sh
@@ -12,6 +12,8 @@ rsync --mkpath -a key       ./dist/staging/config/cabal
 rsync --mkpath -a systemd/  ./dist/staging/config/systemd/user/
 rsync --mkpath -a bin/cabal ./dist/staging/local/bin/cabal
 
+TRANSFORM_THESE='${CABAL_NAMESPACE} ${CABAL_ROOT} ${CABAL_BIN_ROOT} ${CABAL_VERSION} ${CABAL_PORT} ${CABAL_CLUSTER} ${CABAL_LOG_LEVEL}'
+
 ##	build
 cd ./dist/staging
 DEST=../build/linux
@@ -19,5 +21,5 @@ find . -type f | while read fylename;
 do 
 	## inject vars and copy to new folder
 	mkdir -p $(dirname $DEST/$fylename)
-	cat $fylename | envsubst > $DEST/$fylename
+	cat $fylename | envsubst "$TRANSFORM_THESE" > $DEST/$fylename
 done
